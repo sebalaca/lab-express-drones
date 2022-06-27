97c2be18537c970b50b766a2121463f58557b02b
Sebastian Laca <seba.laca@gmail.com>
2022-06-27 16:06:20 +0200

    Iteracion 1 complete

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+const Drone = require("../models/Drone.model");
+
+// insertarla en la BD
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/lab-express-drones")
+  .then((response) => {
+    console.log("conectados a la Dase de Datos");
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  Drone.create(drones)
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    //Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while creating drones from the DB: ${err}`));
